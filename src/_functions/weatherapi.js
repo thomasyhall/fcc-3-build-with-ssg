42cd9ede980cb3b59103cbb7253e08c748fee9e9
Thomas Hall <Thomasyhall@gmail.com>
2020-04-16 15:12:43 -0400

    test

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 exports.handler = async function(event, context, callback) {
 
   const { lat, lon } = event.queryStringParameters;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=784f7efb7e031ca7e85eab7902c9c4d4&units=metric`;
 
   const response = await axios.get(url);
   callback(null, {

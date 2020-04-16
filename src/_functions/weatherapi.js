2f929cc34ce52649e4db6af3f28578436b143d54
Thomas Hall <Thomasyhall@gmail.com>
2020-04-16 15:47:22 -0400

    test

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -1,11 +1,9 @@
 const axios = require("axios");
 
-require('dotenv').config();
-
 exports.handler = async function(event, context, callback) {
 
   const { lat, lon } = event.queryStringParameters;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=imperial`;
 
   const response = await axios.get(url);
   callback(null, {
